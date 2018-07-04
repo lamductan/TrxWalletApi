f4b3a610d6272da8d29a0c62469f3a8bf7e499a2
LAM DUC TAN <tan@LamDucTan.UTS>
2018-07-04 16:48:32 +0700

    Send transaction by ECKey

--- src/org/blockchain/TestWallet.java
+++ src/org/blockchain/TestWallet.java
@@ -1,17 +1,14 @@
-package org.tronUtils;
+package org.blockchain;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tron.api.GrpcAPI;
 import org.tron.common.utils.Utils;
 import org.tron.core.exception.CipherException;
-import org.tron.keystore.StringUtils;
-import org.tron.protos.Protocol;
 import org.tron.protos.Protocol.Account;
 import org.tron.protos.Protocol.Block;
 import org.tron.protos.Protocol.Transaction;
 import org.tron.api.GrpcAPI.BlockList;
-import org.tron.walletserver.WalletClient;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -24,12 +21,12 @@ public class TestWallet {
      * @param blockNum  a number giving height of this block
      */
     public static void getBlock(long blockNum) {
-        Block block = TrxWallet.getBlock(blockNum);
+        Block block = TronUtils.getBlock(blockNum);
         if (block == null) {
             logger.info("No block for num : " + blockNum);
             return;
         }
-        logger.info(Utils.printBlock(block));
+        logger.info(block.toString());
     }
 
     /**
@@ -37,7 +34,7 @@ public class TestWallet {
      * @param transactionId  a string giving string hash of transaction
      */
     public static void getTransactionById(String transactionId) {
-        Optional<Transaction> result = TrxWallet.getTransactionById(transactionId);
+        Optional<Transaction> result = TronUtils.getTransactionById(transactionId);
         if (result.isPresent()) {
             Transaction transaction = result.get();
             logger.info(Utils.printTransaction(transaction));
@@ -48,7 +45,7 @@ public class TestWallet {
 
 
     public static void registerWallet(String password) throws CipherException, IOException {
-        String fileName = TrxWallet.registerWallet(password);
+        String fileName = TronUtils.registerWallet(password);
 
         if (null == fileName) {
             logger.info("Register wallet failed !!");
@@ -58,7 +55,7 @@ public class TestWallet {
     }
 
     public static void getBlockByLimitNext(long startBlockNum, long endBlockNum) {
-        Optional<BlockList> result = TrxWallet.getBlockByLimitNext(startBlockNum, endBlockNum);
+        Optional<BlockList> result = TronUtils.getBlockByLimitNext(startBlockNum, endBlockNum);
         if (result.isPresent()) {
             BlockList blockList = result.get();
             logger.info(Utils.printBlockList(blockList));
@@ -68,7 +65,7 @@ public class TestWallet {
     }
 
     public static void getBlockByLatestNum(long num) {
-        Optional<BlockList> result = TrxWallet.getBlockByLatestNum(num);
+        Optional<BlockList> result = TronUtils.getBlockByLatestNum(num);
         if (result.isPresent()) {
             GrpcAPI.BlockList blockList = result.get();
             logger.info(Utils.printBlockList(blockList));
@@ -78,7 +75,7 @@ public class TestWallet {
     }
 
     public static void getAccount(String accountAddress) {
-        Account account = TrxWallet.getAccount(accountAddress);
+        Account account = TronUtils.getAccount(accountAddress);
         if (account == null) {
             logger.info("GetAccount failed !!!!");
         } else {
@@ -87,7 +84,7 @@ public class TestWallet {
     }
 
     public static void sendCoin(String fromAddress, String password, String walletFilePath, String toAddress, long amount) {
-        boolean result = TrxWallet.sendCoin(fromAddress, password, walletFilePath, toAddress, amount);
+        boolean result = TronUtils.sendCoinFromFileAndPassword(fromAddress, password, walletFilePath, toAddress, amount);
         if (result) {
             logger.info("Send " + amount + " drop to " + toAddress + " successful !!");
         } else {
--- src/org/blockchain/TronUtils.java
+++ src/org/blockchain/TronUtils.java
@@ -1,45 +1,35 @@
-package org.tronUtils;
+package org.blockchain;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.tron.api.GrpcAPI;
+import org.tron.api.GrpcAPI.BlockList;
 import org.tron.common.crypto.ECKey;
-import org.tron.common.crypto.Sha256Hash;
-import org.tron.common.utils.ByteArray;
 import org.tron.common.utils.ByteUtil;
 import org.tron.common.utils.TransactionUtils;
 import org.tron.common.utils.Utils;
-import org.tron.core.exception.CancelException;
 import org.tron.core.exception.CipherException;
 import org.tron.keystore.Credentials;
 import org.tron.keystore.StringUtils;
 import org.tron.keystore.Wallet;
 import org.tron.keystore.WalletFile;
-import org.tron.keystore.WalletUtils;
-import org.tron.protos.Protocol;
+import org.tron.protos.Contract;
 import org.tron.protos.Protocol.Account;
-import org.tron.walletcli.Client;
-import org.tron.walletcli.Test;
-import org.tron.walletserver.GrpcClient;
-import org.tron.walletserver.WalletClient;
 import org.tron.protos.Protocol.Block;
 import org.tron.protos.Protocol.Transaction;
-import org.tron.api.GrpcAPI.BlockList;
-import org.tron.protos.Contract;
+import org.tron.walletserver.GrpcClient;
+import org.tron.walletserver.WalletClient;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Optional;
-import java.util.Scanner;
 
 import static org.tron.keystore.WalletUtils.loadCredentials;
 
-public class TrxWallet {
+public class TronUtils {
     //private static Client client = new Client();
     private static GrpcClient rpcCli = WalletClient.init();
 
-    public static String registerWallet(String passwordStr) throws CipherException, IOException  {
+    public static String registerWallet(String passwordStr) throws CipherException, IOException {
         char[] password = passwordStr.toCharArray();
         if (!WalletClient.passwordValid(password)) {
             return null;
@@ -53,7 +43,7 @@ public class TrxWallet {
     }
 
     public static Block getBlock(long blockNum) {
-       return WalletClient.GetBlock(blockNum);
+        return WalletClient.GetBlock(blockNum);
     }
 
     public static Optional<Transaction> getTransactionById(String transactionId) {
@@ -62,8 +52,9 @@ public class TrxWallet {
 
     /**
      * Print information of all blocks in range(startBlockNum, endBlockNum) inclusive.
-     * @param startBlockNum  a number giving height of start block
-     * @param endBlockNum  a number giving height of end block
+     *
+     * @param startBlockNum a number giving height of start block
+     * @param endBlockNum   a number giving height of end block
      */
     public static Optional<BlockList> getBlockByLimitNext(long startBlockNum, long endBlockNum) {
         return WalletClient.getBlockByLimitNext(startBlockNum, endBlockNum);
@@ -71,7 +62,8 @@ public class TrxWallet {
 
     /**
      * Print information of all latest num blocks.
-     * @param num  a number giving number of latest blocks
+     *
+     * @param num a number giving number of latest blocks
      */
     public static Optional<BlockList> getBlockByLatestNum(long num) {
         return WalletClient.getBlockByLatestNum(num);
@@ -79,30 +71,57 @@ public class TrxWallet {
 
     /**
      * Print information of account.
-     * @param accountAddress  a string giving address of account
+     *
+     * @param accountAddress a string giving address of account
      */
     public static Account getAccount(String accountAddress) {
         byte[] addressBytes = WalletClient.decodeFromBase58Check(accountAddress);
         return WalletClient.queryAccount(addressBytes);
     }
 
-    public static boolean sendCoin(String fromAddress, String password, String walletFilePath, String toAddress, long amount) {
-        byte[] owner = WalletClient.decodeFromBase58Check(fromAddress);
+    public static long getBalance(String address){
+        Account account = getAccount(address);
+        return account.getBalance();
+    }
+
+    public static boolean sendCoinFromFileAndPassword(String fromAddress, String password, String walletFilePath, String toAddress, long amount) {
+
+        try {
+            ECKey ecKey = getEcKey(password, walletFilePath);
+            return sendCoinFromPrivKey(fromAddress, ecKey, toAddress, amount);
+        } catch (Exception e) {
+            System.out.println("wrong sendcoin");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean sendCoinFromPrivKey(String fromAddress, ECKey ecKey, String toAddress, long amount) {
+        if(!WalletClient.encode58Check(ecKey.getAddress()).equals(fromAddress)){
+            System.out.println("from priv key: " + WalletClient.encode58Check(ecKey.getAddress()));
+            System.out.println("address: " + WalletClient.decodeFromBase58Check(fromAddress).toString());
+            System.out.println("address input is not map with private key");
+            return false;
+        }
+        return sendCoin(ecKey, toAddress, amount);
+    }
+
+    public static boolean sendCoin(ECKey ecKey, String toAddress, long amount) {
+        byte[] owner = ecKey.getAddress();
         byte[] to = WalletClient.decodeFromBase58Check(toAddress);
         Contract.TransferContract contract = WalletClient.createTransferContract(to, owner, amount);
         Transaction transaction = rpcCli.createTransaction(contract);
         if (transaction == null) {
             System.out.println("Transaction null");
             return false;
-        }
-        else if (transaction.getRawData().getContractCount() == 0) {
+        } else if (transaction.getRawData().getContractCount() == 0) {
             Logger logger = LoggerFactory.getLogger("TestClient");
             logger.info(Utils.printTransaction(transaction));
             System.out.println("transaction.getRawData().getContractCount() == 0");
             return false;
         }
         try {
-            transaction = TransactionUtils.sign(transaction, getEcKey(password, walletFilePath));
+            transaction = TransactionUtils.sign(transaction, ecKey);
         } catch (Exception e) {
             System.out.println(e);
         }
